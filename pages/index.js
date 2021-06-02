e1e9cd4cbd7bcf121a43baee61f6177c4016f1b3
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-06-02 18:08:45 -0500

    adds styles

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,25 @@ import Header from '../components/header'
 export default function Home() {
   return (
     <>
+      <div className="title">
+        <p>CSS</p>
+      </div>
       <Header />
       <h1>Welcome to Mastering Next.js</h1>
+      <p className="headom">styled-jsx</p>
+      <style jsx>
+        {`
+          .headom {
+            font: 15px Helvetica, Arial, san-serif;
+            background: #eee;
+            padding: 100px;
+            text-align: center;
+          }
+          .headom:hover {
+            background: #ccc;
+          }
+        `}
+      </style>
     </>
   )
 }
