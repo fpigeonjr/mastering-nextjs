31440c43e8a42cb66566a23ac7c8e515a436cfc6
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-06-02 12:31:01 -0500

    fetches dynamic data passed from route

--- pages/post/[id]/index.js
+++ pages/post/[id]/index.js
@@ -2,16 +2,21 @@ import { useRouter } from 'next/router'
 import Header from '../../../components/header'
 import React from 'react'
 
-const Post = () => {
-  const router = useRouter()
-  const { id } = router.query
+const Post = ({ title, body }) => (
+  <React.Fragment>
+    <Header />
+    <h1>{title}</h1>
+    <p>{body}</p>
+  </React.Fragment>
+)
 
-  return (
-    <React.Fragment>
-      <Header />
-      <h1>Post: {id}</h1>
-    </React.Fragment>
+Post.getInitialProps = async ({ query }) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${query.id}`
   )
+  const post = (await res).json()
+
+  return post
 }
 
 export default Post
